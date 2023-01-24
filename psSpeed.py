3d5fdb3a36ecb00dea86c51e9eeec89bc7eb0a4d
LucasMorato <lmorato@gmail.com>
2023-01-24 02:41:54 -0300

    Update psSpeed.py

--- psSpeed.py
+++ psSpeed.py
@@ -1,3 +1,6 @@
+# importar pacotes necessários
+import socket
+import subprocess
 import os
 
 print('Selecione o sistema operacional:')
@@ -6,13 +9,55 @@ print('2 - Linux')
 
 sistema_operacional = int(input('Digite o número: ')) 
 
+
 # Verificando a opção do usuário
 if sistema_operacional == 1:
+    if os.name == 'posix':
+        print('Selecione a opcao Linux. Fechando.')
+        exit(0)
+    else:
+        pass
+    # definir nome do host
+    hostname = socket.gethostname()
+
+    # definir endereços IPV4, IPV6
+    ipv4 = socket.gethostbyname(hostname)
+    command = "ipconfig"
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    ipv6 = ""
+
+    for line in process.stdout.readlines():
+        if "IPv6" in line.decode("utf-8"):
+            ipv6 = line.decode("utf-8").split(" : ")[1].strip()
+
+    # imprimir IPV4, IPV6 e IP do Gateway
+    print("IPV4 deste host '{0}' é {1}".format(hostname, ipv4))
+    print("IPV6 deste host '{0}' é {1}".format(hostname, ipv6))
+    
     # Criando a variável
-    ip_address_w = input("Digite o endereço IP do range, ex. 192.168.0.: ")    
+    ip_address_w = input(f"\nDigite o endereço IP da rede, ex. {ipv4.rsplit('.', 1)[0]}.1: ")    
     os.system(f'for /L %i in (1,1,255) do @ping -n 1 -w 200 {ip_address_w}%i > nul && echo {ip_address_w}%i está ativo.')
 elif sistema_operacional == 2:
-     ip_address_l = input("Digite o endereço IP da rede, ex. 192.168.0.1.: ")
-     os.system(f'fping -ag {ip_address_l}/24 2> /dev/null')
+    if os.name == 'nt':
+        print('Selecione a opcao Windows. Fechando...')
+        exit(0)
+    else:
+        pass
+    # executa o comando ifconfig e salva o resultado em uma variável
+    output = subprocess.check_output(['ifconfig'])
+
+    # Converte o resultado para uma string
+    output = output.decode()
+
+    # Procura pelo IPv4, IPv6 e Gateway
+    ipv4 = output.split('inet')[1].split()[0]
+    ipv6 = output.split('inet6')[1].split()[0]
+
+    # Mostra o resultado
+    print('IPv4:', ipv4)
+    print('IPv6:', ipv6)
+
+    ip_address_l = input(f"\nDigite o endereço IP da rede, ex. {ipv4.rsplit('.', 1)[0]}.1: ")
+    os.system(f'fping -ag {ip_address_l}/24 2> /dev/null')
 else:
     print("Opção inválida!")
